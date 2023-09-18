36a6fe402c3b7197001493489f5742299f80bafc
yanchangxing <ycx1991@live.com>
2023-09-18 16:43:38 +0800

    修改目录

--- src/pages/amap.jsx
+++
@@ -1,9 +0,0 @@
-
-import useAmap from '@/hocs/useAmap'
-
-export default function () {
-    const { mapRef } = useAmap({ id: 'amap-container' })
-    return <div style={{ width: "100%", height: "100vh",paddingTop: '50px',boxSizing:'border-box' }}> 
-        <div id='amap-container' style={{ width: "100%", height: "100%" }}></div>
-    </div>
-}
