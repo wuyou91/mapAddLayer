5c494e3007a82831346ec7b9853eaa62fec4b343
yanchangxing <ycx1991@live.com>
2023-10-12 19:07:11 +0800

    布局修改

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,12 +5,17 @@ import { useEffect } from 'react';
 export default function Layout() {
   return (
     <div className={styles.layout}>
-      <div className={styles.navs}>
-        <Link to='/amap' className={styles.navItem}>高德</Link>
-        <Link to='/cesiumMap' className={styles.navItem}>cesium</Link>
-        <Link to='/test' className={styles.navItem}>test</Link>
+      <div className={styles.navsWrapper}>
+        <div className={styles.navs}>
+          <Link to='/amap' className={styles.navItem}>高德</Link>
+          <Link to='/cesiumMap' className={styles.navItem}>cesium</Link>
+          <Link to='/test' className={styles.navItem}>test</Link>
+          <Link to='/form' className={styles.navItem}>动态表单</Link>
+        </div>
       </div>
+      <div className={styles.main}>
       <Outlet />
+      </div>
     </div>
   );
 }
