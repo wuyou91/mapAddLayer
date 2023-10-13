7bedd539f4cbdd5197f93173d7c7a63593b49622
yanchangxing <ycx1991@live.com>
2023-10-13 15:57:31 +0800

    布局和样式修改

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,26 @@
-import { Link, Outlet } from 'umi';
+import { Link, Outlet, history, useLocation } from 'umi';
 import styles from './index.less';
 import { useEffect } from 'react';
 
 export default function Layout() {
+  const pathName = useLocation().pathname
+  
+  const getClass =(val:String) => {
+    if (pathName === val) {
+      return styles.navItem + ' ' + styles.active
+    } else {
+      return styles.navItem
+    }
+  }
   return (
     <div className={styles.layout}>
       <div className={styles.navsWrapper}>
         <div className={styles.navs}>
-          <Link to='/amap' className={styles.navItem}>高德</Link>
-          <Link to='/cesiumMap' className={styles.navItem}>cesium</Link>
-          <Link to='/test' className={styles.navItem}>test</Link>
-          <Link to='/form' className={styles.navItem}>动态表单</Link>
+          <Link to='/amap' className={getClass('/amap')}>高德</Link>
+          <Link to='/cesiumMap' className={getClass('/cesiumMap')}>cesium</Link>
+          <Link to='/test' className={getClass('/test')}>test</Link>
+          <Link to='/form' className={getClass('/form')}>动态表单</Link>
+          <Link to='/customForm' className={getClass('/customForm')}>自定义动态表单</Link>
         </div>
       </div>
       <div className={styles.main}>
